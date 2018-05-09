f5e1b0bffca0312225be72f39f8077a4baf9cd69
Unknown <spittiforum@googlemail.com>
2018-05-09 16:40:09 +0200

    Added Guava

--- src/ReadXML.java
+++ src/ReadXML.java
@@ -1,6 +1,9 @@
 import java.io.File;
+import java.util.Objects;
+
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
@@ -18,11 +21,42 @@ public class ReadXML {
 			Document doc = dBuilder.parse(XmlFile);
 					 
 			doc.getDocumentElement().normalize();
-			NodeList subList = doc.getElementsByTagName("cim:ACLineSegment");
-			String rdfID;
-			for (int i = 0; i < subList.getLength(); i++) { // … use extractNode method 
-				Element element = (Element) subList.item(1);
-				rdfID = element.getAttribute("rdf:ID");
+			
+			// Convert xml interface document to NodeList containing everything 
+			NodeList xmlNodeList = doc.getElementsByTagName("*");
+		
+		
+			// Loop through nodelist
+			for (int i = 0; i < xmlNodeList.getLength(); i++) {  
+				Element element = (Element) xmlNodeList.item(i);
+				
+				//check if element has rdf:ID
+				if(Objects.equals(element.getAttribute("rdf:ID"), "") == false)
+				{
+					System.out.println("------------------------");
+					System.out.println(element.getTagName());
+					System.out.println(element.getAttribute("rdf:ID"));
+					
+					NodeList NodesOfTagList =  element.getElementsByTagName("*");
+					for (int j= 0; j < NodesOfTagList.getLength(); j++) { // … use extractNode method 
+						Element ElementOfElement = (Element) NodesOfTagList.item(j);
+						
+						if(ElementOfElement.getAttributes().getLength() == 0)
+						{
+							System.out.println(ElementOfElement.getTagName());
+							System.out.println(ElementOfElement.getTextContent());
+						}
+						else
+						{
+							Node attr = ElementOfElement.getAttributes().item(0);
+							System.out.println(ElementOfElement.getTagName() + "_" + attr.getNodeName());
+							System.out.println(attr.getNodeValue().substring(1));
+						}
+						
+						System.out.println("");
+					}
+					
+				}
 			   }
 		}
 		catch(Exception e){
