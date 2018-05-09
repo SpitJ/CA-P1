08b3a550163c567975474bdf43d439084616ff05
Unknown <spittiforum@googlemail.com>
2018-05-09 11:44:49 +0200

    xml read

--- src/ReadXML.java
+++ src/ReadXML.java
@@ -1,4 +1,32 @@
+import java.io.File;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+
 
 public class ReadXML {
-
+	public void ReadXMLinTable()
+	{
+		try {
+			
+			File XmlFile = new File("./xml/Assignment_EQ_reduced.xml");
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(XmlFile);
+					 
+			doc.getDocumentElement().normalize();
+			NodeList subList = doc.getElementsByTagName("cim:ACLineSegment");
+			String rdfID;
+			for (int i = 0; i < subList.getLength(); i++) { // … use extractNode method 
+				Element element = (Element) subList.item(1);
+				rdfID = element.getAttribute("rdf:ID");
+			   }
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
 }
--- src/run.java
+++ src/run.java
@@ -3,7 +3,8 @@ public class run {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ReadXML readxml = new ReadXML();
+		readxml.ReadXMLinTable();
 	}
 
 }
