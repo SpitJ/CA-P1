ba44837be1dae7e8865cff648e56ef7c96e570db
Unknown <spittiforum@googlemail.com>
2018-05-11 18:08:35 +0200

    initial Recursive Function

--- src/CalcYMatrix.java
+++ src/CalcYMatrix.java
@@ -33,8 +33,8 @@ public class CalcYMatrix {
 			{
 				String nodeofterminal = grid.get(terminal, "cim:Terminal.ConnectivityNode_rdf:resource");
 				BusBartoNode.put(bus, nodeofterminal);
-				System.out.println(" Node of Terminal: " + nodeofterminal);
-				System.out.println(" Node of Terminal: " + grid.get(nodeofterminal, "cim:IdentifiedObject.name"));				
+				System.out.println("Node of Terminal: " + nodeofterminal);
+				System.out.println("Node of Terminal: " + grid.get(nodeofterminal, "cim:IdentifiedObject.name"));				
 			}
 			
 			
@@ -44,13 +44,13 @@ public class CalcYMatrix {
 		for (String busbarconnectivitynode : BusBartoNode.values())
 		{
 			// find terminals of busbarconnectivitynode
+			System.out.println("---------------- Starting from new BusBar at origin");
 			Set<String> terminals = getKeysByValue(grid.column("cim:Terminal.ConnectivityNode_rdf:resource"), busbarconnectivitynode);
 			for(String terminal : terminals)
 			{
-//				System.out.println("Node: busbarconnectivitynode " + busbarconnectivitynode + " has Terminal " + terminal);
-				
-				
-				
+				System.out.println("*********Starting from Terminal at origin");
+				analyseNextElement(terminal, busbarconnectivitynode, grid, BusBartoNode);
+//				System.out.println("Node: busbarconnectivitynode " + busbarconnectivitynode + " has Terminal " + terminal);				
 			}
 		}
 //		System.out.println("result of mulitplication = " + multiply_recursive(5,5));
@@ -65,27 +65,51 @@ public class CalcYMatrix {
 	
 	public static void analyseNextElement(String current, String previous, Table<String, String, String> grid, BiMap<String, String> busbartonode)
 	{
+		System.out.println("Analyzing current");
 		 // check if current element is a stop
 		 if(busbartonode.containsKey(current))
 		 {
 			 // current node is a busbar
-			 System.out.println("current node is a busbar: " + current);
+			 System.out.println("current is a busbar: " + current);
+			 return;
 		 }
 		 else if(busbartonode.containsValue(current))
 		 {
 			// current node is a connectivity busbar node
-			 System.out.println("current node is a connectivity busbar node: " + current);
+			 System.out.println("current is a connectivity busbar node: " + current);
+			 return;
 		 }
 		 // TODO: add other stop applicable
 		 
+		 // find out next element and store information of current element
+		 Set<String> possibleNexts = new HashSet<String>();
 		 String TagNamecurrent = grid.get(current, "TagName");
 		 switch(TagNamecurrent)
 		 {
-		 	case "":
-		 		
+		 	case "cim:Terminal":
+		 		possibleNexts.add(grid.get(current, "cim:Terminal.ConductingEquipment_rdf:resource"));
+		 		possibleNexts.add(grid.get(current, "cim:Terminal.ConnectivityNode_rdf:resource"));
+		 		System.out.println("current is a Terminal. Found nexts ");
 		 	break;
+		 	case "cim:ConnectivityNode":
+		 		possibleNexts = getKeysByValue(grid.column("cim:Terminal.ConnectivityNode_rdf:resource"), current);
+		 		System.out.println("current is a ConnectivityNode. Found nexts ");
+		 	break;
+		 	default:
+		 		System.out.println("current is a unkown node. Stopping");
+		 		return;		 		
 		 
 		 }
+		 
+		 // only analyze not previous connected
+		 for(String possibleNext : possibleNexts)
+		 {
+			 if(possibleNext != previous)
+			 {
+				 analyseNextElement(possibleNext, current, grid, busbartonode);
+			 }
+		 }
+		 return;
 	}
 	
 	
