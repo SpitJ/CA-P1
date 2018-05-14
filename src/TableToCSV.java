4a7e37978ec8c24b9f2dd4a73b9d917cccb428ad
Unknown <spittiforum@googlemail.com>
2018-05-14 20:56:59 +0200

    Added comments in csv reader

--- src/TableToCSV.java
+++ src/TableToCSV.java
@@ -1,40 +1,31 @@
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.Writer;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.TreeBasedTable;
 import com.google.common.collect.Table;
 
-
-
 public class TableToCSV {
-	// Calculates a Y Matrix from given Grid information
+	
+	// stores a guava table to a csv
 	public void write(Table<String, String, String> table, String outputFilePath)
 	{   
-		// Print Y matrix as csv
-		
 		try 
 		{
 			Writer out = new BufferedWriter(new FileWriter(outputFilePath));
 			List<String> values = new ArrayList();
 			
+			//Header
 			values.add("");
-//			System.out.println(table.columnKeySet().toString());
 			for (String columnKey : table.columnKeySet())
 			{
 				values.add(columnKey);
 			}
 			writeLine(out, values);
 			values.clear();
+			
+			//rows and values
 			for (String rowKey : table.rowKeySet())
 			{
 				values.add(rowKey);
@@ -45,6 +36,7 @@ public class TableToCSV {
 				writeLine(out, values);
 				values.clear();
 			}
+			
 			out.close();
 		}
 		catch (final Exception e) 
@@ -53,6 +45,7 @@ public class TableToCSV {
 		}
 	}
 	
+	// writes line in csv file with given values
 	public void writeLine(Writer w, List<String> values) 
 	        throws Exception
     {
@@ -72,8 +65,7 @@ public class TableToCSV {
     			}
     			w.write(ch);
           }
-            }
-//            
+            }     
             w.write("\"");
             firstVal = false;
         }
